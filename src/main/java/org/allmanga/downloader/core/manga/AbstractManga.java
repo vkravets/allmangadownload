81de8a634dec3fac62a14ba3ba03ed6a656ae91e
Vladimir Kravets <vova.kravets@gmail.com>
2010-09-27 01:06:15 +0300

    Improve parsing

---
+++ src/main/java/org/allmanga/downloader/core/manga/AbstractManga.java
@@ -0,0 +1,25 @@
+package org.allmanga.downloader.core.manga;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Sly
+ * Date: Sep 27, 2010
+ * Time: 12:35:39 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AbstractManga {
+
+    private MangaCatalog mangaCatalog;
+
+    public AbstractManga(MangaCatalog mangaCatalog) {
+        this.mangaCatalog = mangaCatalog;
+    }
+
+    public MangaCatalog getMangaCatalog() {
+        return mangaCatalog;
+    }
+
+    public void setMangaCatalog(MangaCatalog mangaCatalog) {
+        this.mangaCatalog = mangaCatalog;
+    }
+}
